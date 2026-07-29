a301e95a156d9ae0c74ff3c7df0fdd3bc8282683
Dean-Portfolio <83827362+Dean-Portfolio@users.noreply.github.com>
2026-07-29 17:11:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // Ensures assets resolve from the root domain (https://dean-portfolio.github.io/)
-  base: "/",
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [
     react(),
+    tailwindcss(),
+    tsconfigPaths(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 });
